8548c61b3b6581055d3d27d5c2dac86dda344532
hassangulzar1 <hassangulzar121@gmail.com>
2023-11-23 19:53:37 +0500

    working with navbar

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import classes from "./Navbar.module.css";
+import { karla } from "@/fonts/font";
+const Navbar = () => {
+  return (
+    <nav className={classes.navBackground}>
+      <div className={classes.nav}>
+        <input type="checkbox" className={classes["nav-check"]} />
+        <div className={classes["nav-header"]}>
+          <div className={classes["nav-title"]}>Hassan</div>
+        </div>
+        <div className={classes["nav-btn"]}>
+          <label for="nav-check">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+        </div>
+
+        <div className={classes["nav-links"]}>
+          <a target="_blank" className={` p-6`}>
+            HOME
+          </a>
+          <a target="_blank" className={` p-6`}>
+            ABOUT
+          </a>
+          <a target="_blank" className={` p-6`}>
+            SERVICES
+          </a>
+          <a target="_blank" className={` p-6`}>
+            PORTFOLIO
+          </a>
+          <a target="_blank" className={` p-6`}>
+            CONTACT
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ fonts/font.js
@@ -0,0 +1,18 @@
+// import { Inter, Poppins, Karla } from "next/font/google";
+//
+// export const inter = Inter({
+//   subsets: ["latin"],
+//   display: "swap",
+// });
+// export const karla = Karla({
+//   weight: ["700"],
+//   subsets: ["latin"],
+//   display: "swap",
+//   variable: "--font-karla",
+// });
+//
+// export const poppins = Poppins({
+//   weight: ["100", "200", "300", "500", "700"],
+//   subsets: ["latin"],
+//   display: "swap",
+// });

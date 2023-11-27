14e11580a7284c726328775cb642156ff45bc496
hassangulzar1 <hassangulzar121@gmail.com>
2023-11-27 22:54:48 +0500

    card style selected

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,5 +1,6 @@
 import DownNavbar from "@/components/downNavbar/DownNavbar";
 import classes from "./Portfolio.module.css";
+import PortfolioCard from "@/components/portfolioCard/PortfolioCard";
 
 export default function page() {
   return (
@@ -20,15 +21,15 @@ export default function page() {
             Recent Work
           </h1>
 
-          <div
-            className={
-              " h-96 ms-5 mt-6 flex gap-7 sm:gap-10 " + classes.worksDiv
-            }
-          >
+          <div className={"ms-5 mt-6 flex gap-7 sm:gap-10 " + classes.worksDiv}>
             <li className="text-xl">All</li>
             <li className="text-xl">Web Designs</li>
             <li className="text-xl">Front-End</li>
             <li className="text-xl">Full-Stack</li>
+          </div>
+
+          <div className="my-20 mx-10">
+            <PortfolioCard />
           </div>
         </div>
       </div>

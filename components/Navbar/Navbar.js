377050094bbdefdf9c6f56a449a0f18c62336722
hassangulzar1 <hassangulzar121@gmail.com>
2023-11-30 18:58:34 +0500

    navbar dropdown works

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,20 +1,29 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Navbar.module.css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const Navbar = () => {
   const pathname = usePathname();
+  const [showDropdown, setshowDropdown] = useState(false);
+
   return (
     <nav className={`${classes.navBackground} z-10`}>
       <div className={classes.nav}>
-        <input type="checkbox" className={classes["nav-check"]} />
+        <input
+          type="checkbox"
+          checked={showDropdown}
+          className={classes["nav-check"]}
+        />
         <div className={classes["nav-header"]}>
           <div className={classes["nav-title"]}>&#128075;</div>
         </div>
-        <div className={classes["nav-btn"]}>
-          <label for="nav-check">
+        <div
+          className={classes["nav-btn"]}
+          onClick={() => setshowDropdown((prevState) => !prevState)}
+        >
+          <label htmlFor={classes["nav-check"]}>
             <span></span>
             <span></span>
             <span></span>
@@ -24,6 +33,7 @@ const Navbar = () => {
         <div className={classes["nav-links"]}>
           <Link
             href="/"
+            onClick={() => setshowDropdown(false)}
             className={`${pathname === "/" ? classes["active"] : ""}`}
             style={{ padding: "20px" }}
           >
@@ -31,6 +41,7 @@ const Navbar = () => {
           </Link>
           <Link
             href="/about"
+            onClick={() => setshowDropdown(false)}
             className={`${pathname === "/about" ? classes["active"] : ""}`}
             style={{ padding: "20px" }}
           >
@@ -38,6 +49,7 @@ const Navbar = () => {
           </Link>
           <Link
             href="/services"
+            onClick={() => setshowDropdown(false)}
             className={`${pathname === "/services" ? classes["active"] : ""}`}
             style={{ padding: "20px" }}
           >
@@ -45,6 +57,7 @@ const Navbar = () => {
           </Link>
           <Link
             href="/portfolio"
+            onClick={() => setshowDropdown(false)}
             className={`${pathname === "/portfolio" ? classes["active"] : ""}`}
             style={{ padding: "20px" }}
           >
@@ -52,6 +65,7 @@ const Navbar = () => {
           </Link>
           <Link
             href="/contact"
+            onClick={() => setshowDropdown(false)}
             className={`${pathname === "/contact" ? classes["active"] : ""}`}
             style={{ padding: "20px" }}
           >

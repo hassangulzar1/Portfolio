819716af23cf5d9f06cc06fadfcb5ddf3fa01815
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-01 23:37:24 +0500

    fixing

--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@
 
 module.exports = {
   images: {
-    remotePatterns: [
-      {
-        hostname: "images.ctfassets.net",
-      },
-    ],
+    domains: ["images.ctfassets.net"],
+    deviceSizes: [320, 420, 768, 1024, 1200],
   },
 };
